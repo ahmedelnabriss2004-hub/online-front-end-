abd807833ef0e2a91f9c382adee6147b52faf509
ahmedelnabriss2004-hub <ahmedelnabriss2004@gmail.com>
2026-05-09 23:00:13 +0300

    js add element to html

---
+++ starter copy 5/js/apps.js
@@ -0,0 +1,40 @@
+const have_children = document.querySelector("#have_children");
+const children_wrapper = document.querySelector(".children-info")
+
+have_children.onclick = () => {
+    if (have_children.checked) {
+        children_wrapper.style.display = "block";
+    } else {
+        children_wrapper.style.display = "none";
+    }
+}
+
+const btn = document.querySelector(".add-child");
+const wrapper = document.querySelector(".children-wrapper");
+let counter = 2;
+
+btn.onclick = (e) => {
+    e.preventDefault();
+    let item = ` <div class="child">
+                        <h3>الابن #${counter}</h3>
+                        <div>
+                            <label for="c${counter}name">الاسم</label>
+                            <input type="text" name="c${counter}name" id="c${counter}name">
+                        </div>
+                        <div> <label for="c${counter}id">رقم الهوية</label>
+                            <input type="number" name="c${counter}id" id="c${counter}id">
+                        </div>
+
+                        <div>
+                            <label for="c${counter}age">السن</label>
+                            <input type="number" name="c${counter}age" id="c${counter}age">
+                        </div>
+                        
+                    </div>`
+
+
+
+    wrapper.insertAdjacentHTML('beforeend', item);
+    counter++;
+
+}
